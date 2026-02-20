f34f28630dc7194b156993594d76112727b35e4e
Disitha395ra <disithar@gmail.com>
2026-02-20 19:26:04 +0530

    added firebase configurations

---
+++ gas-admin/src/config/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import Constants from "expo-constants";
+
+const {
+  firebaseApiKey,
+  firebaseAuthDomain,
+  firebaseDatabaseURL,
+  firebaseProjectId,
+  firebaseStorageBucket,
+  firebaseMessagingSenderId,
+  firebaseAppId
+} = Constants.expoConfig.extra;
+
+const firebaseConfig = {
+  apiKey: firebaseApiKey,
+  authDomain: firebaseAuthDomain,
+  databaseURL: firebaseDatabaseURL,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
---
+++ gas-customer/app.config.js
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "gas-customer",
+    slug: "gas-customer",
+    version: "1.0.0",
+    extra: {
+      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseDatabaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    }
+  }
+};
